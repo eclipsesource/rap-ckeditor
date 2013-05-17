df10858cf567a28354b72fbde65b656d3b5712c8
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-05-17 17:00:31 +0200

    Rename bundles/packages to org.eclipse.rap.addons.*

---
+++ bundles/org.eclipse.rap.addons.ckeditor/src/org/eclipse/rap/addons/ckeditor/CKEditor.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.addons.ckeditor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.eclipse.rap.json.JsonObject;
+import org.eclipse.rap.json.JsonValue;
+import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.client.service.JavaScriptLoader;
+import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
+import org.eclipse.rap.rwt.remote.AbstractOperationHandler;
+import org.eclipse.rap.rwt.remote.Connection;
+import org.eclipse.rap.rwt.remote.OperationHandler;
+import org.eclipse.rap.rwt.remote.RemoteObject;
+import org.eclipse.rap.rwt.service.ResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Layout;
+
+
+public class CKEditor extends Composite {
+
+  private static final String RESOURCES_PATH = "resources/";
+  private static final String REGISTER_PATH = "ckeditor/";
+
+  private static final String[] RESOURCE_FILES = {
+    "ckeditor.js",
+    "config.js",
+    "handler.js",
+    "contents.css",
+    "lang/en.js",
+    "skins/kama/editor.css",
+    "skins/kama/icons.png",
+    "skins/kama/images/sprites.png",
+    "skins/kama/images/sprites_ie6.png"
+  };
+  private static final String REMOTE_TYPE = "eclipsesource.CKEditor";
+
+  private String text = "";
+  private final RemoteObject remoteObject;
+
+  private final OperationHandler operationHandler = new AbstractOperationHandler() {
+    @Override
+    public void handleSet( JsonObject properties ) {
+      JsonValue textValue = properties.get( "text" );
+      if( textValue != null ) {
+        text = textValue.asString();
+      }
+    }
+  };
+
+  public CKEditor( Composite parent, int style ) {
+    super( parent, style );
+    registerResources();
+    loadJavaScript();
+    Connection connection = RWT.getUISession().getConnection();
+    remoteObject = connection.createRemoteObject( REMOTE_TYPE );
+    remoteObject.setHandler( operationHandler );
+    remoteObject.set( "parent", WidgetUtil.getId( this ) );
+  }
+
+  private void registerResources() {
+    ResourceManager resourceManager = RWT.getResourceManager();
+    boolean isRegistered = resourceManager.isRegistered( REGISTER_PATH + RESOURCE_FILES[ 0 ] );
+    if( !isRegistered ) {
+      try {
+        for( String fileName : RESOURCE_FILES ) {
+          register( resourceManager, fileName );
+        }
+      } catch( IOException ioe ) {
+        throw new IllegalArgumentException( "Failed to load resources", ioe );
+      }
+    }
+  }
+
+  private void loadJavaScript() {
+    JavaScriptLoader jsLoader = RWT.getClient().getService( JavaScriptLoader.class );
+    ResourceManager resourceManager = RWT.getResourceManager();
+    jsLoader.require( resourceManager.getLocation( REGISTER_PATH + "handler.js" ) );
+    jsLoader.require( resourceManager.getLocation( REGISTER_PATH + "ckeditor.js" ) );
+    jsLoader.require( resourceManager.getLocation( REGISTER_PATH + "config.js" ) );
+  }
+
+  private void register( ResourceManager resourceManager, String fileName ) throws IOException {
+    ClassLoader classLoader = CKEditor.class.getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream( RESOURCES_PATH + fileName );
+    try {
+      resourceManager.register( REGISTER_PATH + fileName, inputStream );
+    } finally {
+      inputStream.close();
+    }
+  }
+
+  ////////////////////
+  // overwrite methods
+
+  @Override
+  public void setLayout( Layout layout ) {
+    throw new UnsupportedOperationException( "Cannot change internal layout of CkEditor" );
+  }
+
+  @Override
+  public void setFont( Font font ) {
+    super.setFont( font );
+    remoteObject.set( "font", getCssFont() );
+  }
+
+  @Override
+  public void dispose() {
+    remoteObject.destroy();
+    super.dispose();
+  }
+
+  //////
+  // API
+
+  public void setText( String text ) {
+    checkWidget();
+    if( text == null ) {
+      SWT.error( SWT.ERROR_NULL_ARGUMENT );
+    }
+    this.text = text;
+    remoteObject.set( "text", text );
+  }
+
+  public String getText() {
+    checkWidget();
+    return text;
+  }
+
+
+  private String getCssFont() {
+    StringBuilder result = new StringBuilder();
+    if( getFont() != null ) {
+      FontData data = getFont().getFontData()[ 0 ];
+      result.append( data.getHeight() );
+      result.append( "px " );
+      result.append( data.getName() );
+    }
+    return result.toString();
+  }
+
+}
---
+++ tests/org.eclipse.rap.addons.ckeditor.test/src/org/eclipse/rap/addons/ckeditor/CKEditor_Test.java
@@ -0,0 +1,152 @@
+package org.eclipse.rap.addons.ckeditor;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import junit.framework.TestCase;
+
+import org.eclipse.rap.addons.ckeditor.CKEditor;
+import org.eclipse.rap.json.JsonObject;
+import org.eclipse.rap.json.JsonValue;
+import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.client.WebClient;
+import org.eclipse.rap.rwt.client.service.JavaScriptLoader;
+import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
+import org.eclipse.rap.rwt.remote.Connection;
+import org.eclipse.rap.rwt.remote.OperationHandler;
+import org.eclipse.rap.rwt.remote.RemoteObject;
+import org.eclipse.rap.rwt.service.ResourceManager;
+import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.mockito.ArgumentCaptor;
+
+
+public class CKEditor_Test extends TestCase {
+
+  private Display display;
+  private Shell shell;
+  private CKEditor editor;
+  private Connection connection;
+  private RemoteObject remoteObject;
+
+  @Override
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+    display = new Display();
+    shell = new Shell( display );
+    Fixture.fakeNewRequest();
+    remoteObject = mock( RemoteObject.class );
+    connection = mock( Connection.class );
+    when( connection.createRemoteObject( anyString() ) ).thenReturn( remoteObject );
+    Fixture.fakeConnection( connection );
+    editor = new CKEditor( shell, SWT.BORDER );
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    Fixture.tearDown();
+  }
+
+  public void testSetLayout() {
+    try {
+      editor.setLayout( new FillLayout() );
+      fail();
+    } catch( UnsupportedOperationException ex ) {
+      // expected
+    }
+  }
+
+  public void testContructor_CreatesRemoteObjectWithCorrectType() {
+    verify( connection ).createRemoteObject( eq( "eclipsesource.CKEditor" ) );
+  }
+
+  public void testContructor_SetsParent() {
+    verify( remoteObject ).set( "parent", WidgetUtil.getId( editor ) );
+  }
+
+  public void testContructor_LoadsJavaScriptFiles() {
+    JavaScriptLoader loader = mockJavaScriptLoader();
+    ResourceManager resourceManager = RWT.getResourceManager();
+
+    new CKEditor( shell, SWT.BORDER );
+
+    verify( loader ).require( resourceManager.getLocation( "ckeditor/ckeditor.js" ) );
+    verify( loader ).require( resourceManager.getLocation( "ckeditor/config.js" ) );
+    verify( loader ).require( resourceManager.getLocation( "ckeditor/handler.js" ) );
+  }
+
+  public void testSetText_GetText() {
+    String text = "foo<span>bar</span>";
+
+    editor.setText( text );
+
+    assertEquals( text, editor.getText() );
+  }
+
+  public void testSetText_RendersToClient() {
+    String text = "foo<span>bar</span>";
+
+    editor.setText( text );
+
+    verify( remoteObject ).set( "text", text );
+  }
+
+  public void testSetTextNull() {
+    try {
+      editor.setText( null );
+      fail();
+    } catch( IllegalArgumentException ex ) {
+      // expected
+    }
+  }
+
+  public void testSetTextFromClient() {
+    String text = "foo<span>bar</span>";
+
+    remoteSet( remoteObject, "text", JsonValue.valueOf( text ) );
+
+    assertEquals( text, editor.getText() );
+  }
+
+
+  public void testSetFont_RendersToClient() {
+    editor.setFont( new Font( display, "fantasy", 13, 0 ) );
+
+    verify( remoteObject ).set( "font", "13px fantasy" );
+  }
+
+  public void testDispose_RendersDestroyToClient() {
+    editor.dispose();
+
+    verify( remoteObject ).destroy();
+  }
+
+  /////////
+  // Helper
+
+  private JavaScriptLoader mockJavaScriptLoader() {
+    WebClient client = mock( WebClient.class );
+    Fixture.fakeClient( client );
+    JavaScriptLoader loader = mock( JavaScriptLoader.class );
+    when( client.getService( JavaScriptLoader.class ) ).thenReturn( loader );
+    return loader;
+  }
+
+  private static void remoteSet( RemoteObject remoteObjectMock, String proprety, JsonValue value ) {
+    getHandler( remoteObjectMock ).handleSet( new JsonObject().add( proprety, value ) );
+  }
+
+  private static OperationHandler getHandler( RemoteObject remoteObjectMock ) {
+    ArgumentCaptor<OperationHandler> captor = ArgumentCaptor.forClass( OperationHandler.class );
+    verify( remoteObjectMock ).setHandler( captor.capture() );
+    return captor.getValue();
+  }
+
+}
