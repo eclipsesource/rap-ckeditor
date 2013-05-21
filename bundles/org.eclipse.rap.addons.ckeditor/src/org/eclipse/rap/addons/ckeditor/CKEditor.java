e40e350f34f8588a88cde5c8e7e23a9b8b80d343
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-05-21 11:55:16 +0200

    Update copyright headers

--- bundles/org.eclipse.rap.addons.ckeditor/src/org/eclipse/rap/addons/ckeditor/CKEditor.java
+++ bundles/org.eclipse.rap.addons.ckeditor/src/org/eclipse/rap/addons/ckeditor/CKEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 EclipseSource and others.
+ * Copyright (c) 2011, 2013 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@ package org.eclipse.rap.addons.ckeditor;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import org.eclipse.rap.json.JsonObject;
 import org.eclipse.rap.json.JsonValue;
 import org.eclipse.rap.rwt.RWT;
